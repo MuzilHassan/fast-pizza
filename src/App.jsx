32ff7d6c7fc25302c2df2de2b6bcd5ecb73789e3
Muziljohn <muzilhassan9@gmail.com>
2024-12-30 13:06:58 +0500

    added data fetching using react router 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Applayout from "./ui/Applayout";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 // import Order from "./features/order/Order";
 // import CreateOrder from "./features/order/CreateOrder";
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader,
       },
       {
         path: "/cart",
